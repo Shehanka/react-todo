5ea7d5418b91347fd289412fa45944fafa4565db
Shehanka <hcsperera@outlook.com>
2019-11-06 00:34:40 +0530

    bootstrap bug fixed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-//   <BrowserRouter>
-    <App />
-//   </BrowserRouter>
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
